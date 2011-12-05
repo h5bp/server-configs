fb4c6ec6fc1e5fa5a8d2b265f617b25cd937743d
Nick Baugh <niftylettuce@gmail.com>
2011-12-04 20:21:32 -0500

    Updated docs, updated file per call with @xonecas

--- node.js
+++ node.js
@@ -1,94 +1,83 @@
 
-//     node.js server config for html5-boilerplate
-//       by @niftylettuce and @xonecas
+// # [node.js h5bp server-config](https://github.com/h5bp/server-configs)
+//  by @xonecas and @niftylettuce
 
-// # node.js html5-boilerplate server-config
+// Requires latest version of `node` and `npm`
+// <https://gist.github.com/579814>
 
-// Requirements:
-// * You need to install node and npm first!
+// # Getting started:
+// * Create directories `$ mkdir -p ~/project/node_modules && cd ~/project`
+// * Install dependencies `$ npm install connect colors`
 
-// Instructions:
-
-//      # create project directory and node_modules folder
-//      mkdir -p ~/project-name/node_modules
-//      cd ~/project-name
-//
-//      # install connect
-//      npm install connect
-//
-//      # start server
-//      node node.js
+// # Usage:
+// `$ PRODUCTION=true node node.js` (prod mode) or `$ node node.js` (dev mode)
 
 var connect  = require('connect')
+  , colors = require('colors')
   , cacheAge = 24 * 60 * 60 * 1000
-    // USAGE:
-    // prod -> PRODUCTION=true node server.js
-    // dev -> node server
   , prod = process.env.PRODUCTION
-  , root = prod ? 'path/to/prod/files': 'path/to/dev/files'
+  // **NOTE:** You'll need to change these paths:
+  , root = prod ? 'path/to/prod/public': 'path/to/dev/public'
   , port = prod ? 80 : 8080;
 
 // # Routes
 var routes = function(app) {
 
-  // TODO: Insert your routes here (e.g. app.get('/home') app.post('/form'))
+  // **TODO:** Insert your routes here (e.g. app.get('/home') app.post('/form'))
 
   // ## Always keep this route last
   app.get('*', function(req, res, next) {
 
-    // ## Better website experience for IE users
     var url = req.url
-      , ua = req.headers['user-agent']
-      , reqPath = req.url;
+      , ua = req.headers['user-agent'];
 
-    if(ua && ua.indexOf('MSIE'))
+    // ## Block access to hidden files and directories that begin with a period
+    if (url.match(/(^|\/)\./)) {
+      res.end("Not allowed");
+    }
+
+    // ## Better website experience for IE users
+    //  Force the latest IE version, in cases when it may fall back to IE7 mode
+    if(ua && ua.indexOf('MSIE') && /htm?l/.test(ua)) {
       res.setHeader('X-UA-Compatible', 'IE=Edge,chrome=1');
-
-    // We don't want to send this header on _everything_
-    if(reqPath.match(/\.(js|css|gif|png|jpe?g|pdf|xml|oga|ogg|m4a|ogv|mp4|m4v|webm|svg|svgz|eot|ttf|otf|woff|ico|webp|appcache|manifest|htc|crx|xpi|safariextz|vcf)$/)) {
-      res.removeHeader('X-UA-Compatible');
-      res.removeHeader('IE=Edge,chrome=1');
     }
 
     // ## CORS
-    //  Force the latest IE version, in cases when it may fall back to IE7 mode
     //  <http://github.com/rails/rails/commit/123eb25#commitcomment-118920>
     //  Use ChromeFrame if it's installed, for a better experience with IE folks
     //  Control cross domain using CORS http://enable-cors.org
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
 
-    // NOTE: gzippo _should_ only be used in a production environment
-    //  <https://github.com/tomgallacher/gzippo>
-    // TODO: If you want to enable compression, then add the following require:
-    //  var gzippo = require('gzippo'); // you'll also need to `$ npm install gzippo`
-    // TODO: Then add the following inside var routes {} below:
-    //  app.use(gzippo.staticGzip(htdocs, { maxAge: cacheAge }));
-    // TODO: You'll also need to remove the normal connect.static below
+    // ## Production Environment Compression (requires latest version of node)
+    //
+    // If you want to enable [gzippo](<https://github.com/tomgallacher/gzippo>):
+    //
+    //   `var gzippo = require('gzippo');'`
+    //
+    // Then install gzippo via `$ npm install gzippo` and add to `var routes{}`:
+    //
+    //   `app.use(gzippo.staticGzip(root, { maxAge: cacheAge}));`
+    //
+    // Next remove `, connect["static"](root, { maxAge: cacheAge })` from below.
 
-    // TODO: Allow concatenation from within specific js and css files
+    // **TODO:** Allow concatenation from within specific js and css files
     // https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L111
 
-    // TODO: Stop screen flicker in IE on CSS rollovers
+    // **TODO:** Stop screen flicker in IE on CSS rollovers
     // https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L271
 
-    // TODO: Cookie setting from iframes
+    // **TODO:** Cookie setting from iframes
     // https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L286
 
-    // TODO: Suppress or force the "www." at the beginning of URLs
+    // **TODO:** Suppress or force the "www." at the beginning of URLs
     // https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L315
 
-    // TODO: Built-in filename-based cache busting
+    // **TODO:** Built-in filename-based cache busting
     // https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L356
 
-    // TODO: Prevent SSL cert warnings
+    // **TODO:** Prevent SSL cert warnings
     // https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L376
-
-    // TODO: UTF-8 encoding
-    // https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L411
-
-    // TODO: Block access to "hidden" directories whose names begin with a period
-    // https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L442
 
     next();
   });
@@ -97,7 +86,7 @@ var routes = function(app) {
 
 // ## Start the server
 var server = connect.createServer(
-    // This hawt logger is from Expressling
+    // Logger from [Expressling](https://github.com/niftylettuce/expressling)
     connect.logger(''
       + '\\n  ' + ':date'.bold.underline + '\\n\\n' + '  IP: '.cyan.bold
       + ' ' + ':remote-addr'.white + '\\n' + '  Method: '.red.bold
@@ -108,4 +97,4 @@ var server = connect.createServer(
   , connect["static"](root, { maxAge: cacheAge })
 );
 server.listen(port);
-console.log('\n  ' + 'NODE UP ON PORT '.rainbow.bold + port); // aww pretty!
+console.log('\n  ' + 'NODE UP ON PORT '.rainbow + port);
