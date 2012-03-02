d7a87d51703e29e3f44215620acb5181287a697c
Sean Caetano Martin <seancaetanomartin@gmail.com>
2012-03-02 10:17:57 -0800

    rewrite of the file according to the discussion on issue #46

--- node.js
+++ node.js
@@ -1,82 +1,77 @@
-// # [node.js h5bp server-config](https://github.com/h5bp/server-configs)
-//  by @xonecas and @niftylettuce
 
-// Requires latest version of `node` and `npm`
-// <https://gist.github.com/579814>
 
-// # Getting started:
-// * Create directories `$ mkdir -p ~/project/node_modules && cd ~/project`
+var h5bp = modules.exports,
+   _http = require('http');
 
-// # Usage:
-// `$ PRODUCTION=true node node.js` (prod mode) or `$ node node.js` (dev mode)
+h5bp = {
+   // send the IE=Edge and chrome=1 headers for IE browsers
+   // on html/htm resquests.
+   ieEdgeChromeFrameHeader: function () {
+      return function (req, res, next) {
+         var url = req.url,
+            ua = req.headers['user-agent'];
 
-var connect  = require('connect'),
-   cacheAge = 24 * 60 * 60 * 1000,
-   prod = process.env.PRODUCTION,
-   // specify a production directory, or serve current directory.
-   root = prod ? 'path/to/prod/public': __dirname,
-   port = prod ? 80 : 8080;
+         if (ua && ua.indexOf('MSIE') && /html?$/.test(url)) {
+            res.setHeader('X-UA-Compatible', 'IE=Edge,chrome=1');
+         }
+         next();
+      };
+   },
 
-// # Routes
-var routes = function(app) {
+   // block access to hidden files and directories.
+   protectDotfiles: function () {
+      return function (req, res, next) {
+         var error;
+         if (/(^|\/)\./.test(req.url)) {
+            error = new Error(_http.STATUS_CODES[405]); // 405, not allowed
+            error.status = 405;
+         }
+         next(error);
+      };
+   },
 
-   // # Insert your routes here (e.g. app.get('/home') app.post('/form'))
+   // Enable CORS cross domain rules, more info at http://enble-cors.org/
+   crossDomainRules: function () {
+      return function (req, res, next) {
+         res.setHeader('Access-Control-Allow-Origin', '*');
+         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With');
+         next();
+      };
+   },
 
-   // ## Always keep this route last
-   app.get('*', function(req, res, next) {
-      var url = req.url,
-         ua = req.headers['user-agent'];
+   // Suppress or force 'www' in the urls
+   // @param suppress = boolean
+   suppressWww: function (suppress) {
+      return function (req, res, next) {
+         var url = req.url;
+         if (suppress && /^www\./.test(url)) {
+            res.status = 302;
+            res.setHeader('Location', url.replace(/^www\./,''));
+         }
+         if (!suppress && !/^www\./.test(url)) {
+            res.status = 302;
+            res.setHeader('Location', "www."+url);
+         }
+         next();
+      };
+   },
 
-      // ## Block access to hidden files and directories that begin with a period
-      if (url.match(/(^|\/)\./)) {
-         res.end("Not allowed");
-      }
-
-      // ## Better website experience for IE users
-      //  Force the latest IE version, in cases when it may fall back to IE7 mode
-      if(ua && ua.indexOf('MSIE') && /htm?l$/.test(url)) {
-         res.setHeader('X-UA-Compatible', 'IE=Edge,chrome=1');
-      }
-
-      //  Control cross domain using CORS http://enable-cors.org
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
-
-      // **TODO:** Allow concatenation from within specific js and css files
-      // https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L111
-
-      // **TODO:** Stop screen flicker in IE on CSS rollovers
-      // https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L271
-
-      // **TODO:** Cookie setting from iframes
-      // https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L286
-
-      // **TODO:** Suppress or force the "www." at the beginning of URLs
-      // https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L315
-      if (/^www\./.test(url)) {
-         res.statusCode = 302;
-         res.setHeader('Location', url.replace(/^www\./, ''));
-         res.end();
-      }
-
-      // **TODO:** Built-in filename-based cache busting
-      // https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L356
-
-      // **TODO:** Prevent SSL cert warnings
-      // https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L376
-
-      next();
-   });
-
+   // return a express/connect server with the default middlewares.
+   // @param serverConstructor = express/connect server instance
+   server: function (serverContructor) {
+      return serverContructor.createServer(
+         serverContructor.logger('dev'),
+         this.ieEdgeChromeFrameHeader(),
+         this.protectDotfiles(),
+         this.crossDomainRules(),
+         this.suppressWww(true),
+         serverContructor['static'](__.dirname), // static is a reserved keyword
+         serverContructor.favicon(__dirname),
+         serverContructor.errorHandler({
+            stack: true,
+            message: true,
+            dump: true
+         })
+      );
+   }
 };
-
-// ## Start the server
-var server = connect.createServer(
-   connect.logger('dev'),
-   connect.router(routes),
-   // if you want gzip, then replace the next line with gzippo
-   // https://github.com/tomgallacher/gzippo
-   connect["static"](root, { maxAge: cacheAge })
-);
-server.listen(port);
-console.log('NODE UP ON PORT '+ port);
